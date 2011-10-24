58c71df4fb9353131f3fe8c176054d3c8cdde12f
Bob Breznak <bob.breznak@gmail.com>
2011-10-24 07:10:17 -0400

    Mispelling bug

--- lib/swallow/deploy.rb
+++ lib/swallow/deploy.rb
@@ -165,7 +165,7 @@ Capistrano::Configuration.instance(true).load do
   namespace :unicorn do
     desc "Creates a symlink to the unicorn management script"
     task :create_symlink, :roles => :app do
-      run "ln -s /etc/init.d/unicorn #{shared_path}/system/#{appliction}"
+      run "ln -s /etc/init.d/unicorn #{shared_path}/system/#{application}"
     end
   end
 
