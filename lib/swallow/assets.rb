d37bb7d874f5f6726bc18c2543fff5e96ced3d30
Bob Breznak <bob.breznak@gmail.com>
2011-12-11 19:21:14 -0500

    Adding bundle exec back in for asset sync... I think that I'll need to do this for hoptoad too

--- lib/swallow/assets.rb
+++ lib/swallow/assets.rb
@@ -4,7 +4,7 @@ Capistrano::Configuration.instance(true).load do
   namespace :assets do
     task :sync, :roles => :app do
       run "cd #{release_path} && source .rvmrc && RAILS_ENV=#{rails_env} rake asset:id:upload" if uses_asset_id
-      run "cd #{release_path} && source .rvmrc && RAILS_ENV=#{rails_env} rake assets:precompile" if uses_asset_sync
+      run "cd #{release_path} && source .rvmrc && RAILS_ENV=#{rails_env} bundle exec rake assets:precompile" if uses_asset_sync
     end
   end
 
