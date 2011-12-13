7f1c07f1b5217f1a47005609f7bbdfb38a60545b
Bob Breznak <bob.breznak@gmail.com>
2011-12-13 10:01:25 -0500

    Filtering output of asset:precompile

--- lib/swallow/assets.rb
+++ lib/swallow/assets.rb
@@ -3,7 +3,12 @@ Capistrano::Configuration.instance(true).load do
   desc "Automatically called as apart of a standard deploy, unless there is a `no_asset_id` configuration. Runs the rake task asset:id:upload."
   namespace :assets do
     task :sync, :roles => :app do
-      run "cd #{release_path} && source .rvmrc && RAILS_ENV=#{rails_env} bundle exec rake assets:precompile" if use_asset_sync
+      if use_asset_sync
+        run "cd #{release_path} && source .rvmrc && RAILS_ENV=#{rails_env} bundle exec rake assets:precompile" do |chan, stream, data|
+          puts "  * [#{chan[:host]}] #{data}" if data.match(/^\s*(Using|Uploading)/)
+        end
+      end
+
       run "cd #{release_path} && source .rvmrc && RAILS_ENV=#{rails_env} bundle exec rake asset:id:upload" if use_asset_id
 
     end
