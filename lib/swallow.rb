6b5572da6839e66a76933dc939f74400654ba7a1
Ken Mazaika <kenmazaika@gmail.com>
2012-01-25 14:13:20 -0500

    daemonize the deploy.

--- lib/swallow.rb
+++ lib/swallow.rb
@@ -2,5 +2,12 @@ unless Capistrano::Configuration.respond_to?(:instance)
   abort "capistrano/ext/multistage requires Capistrano 2"
 end
 
-Dir[File.join(File.dirname(__FILE__), 'swallow/*.rb')].sort.each { |lib| require lib }
+# Require the Deploy Hook Files, if they are present
+['after_symlink.rb'].each do |script_name|
+  script_file_path = "#{Dir.pwd}/hooks/#{script_name}"
+  if File.exist?(script_file_path)
+    require script_file_path
+  end
+end
 
+Dir[File.join(File.dirname(__FILE__), 'swallow/*.rb')].sort.each { |lib| require lib }
