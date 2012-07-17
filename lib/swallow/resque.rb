3a579435e91cf760dd2e749c1b21138f8b27e599
JohnAmican <amicangelo.j@husky.neu.edu>
2012-07-17 15:21:20 -0400

    forgot the word rake god damnit

--- lib/swallow/resque.rb
+++ lib/swallow/resque.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(true).load do
   namespace :deploy do
     desc "Restart Resque Workers"
     task :restart_workers, :roles => :daemon do
-       run "#{source_rvmrc} && RAILS_ENV=#{rails_env} bundle exec resque:restart_workers" if use_resque
+       run "#{source_rvmrc} && RAILS_ENV=#{rails_env} bundle exec rake resque:restart_workers" if use_resque
     end
   end
   
