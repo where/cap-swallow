15a1fecddb5c45907689dccd327fc5d42fae83e2
JohnAmican <amicangelo.j@husky.neu.edu>
2012-07-17 15:14:15 -0400

    added conditional to make sure resque is enabled

--- lib/swallow/resque.rb
+++ lib/swallow/resque.rb
@@ -1,17 +1,8 @@
 Capistrano::Configuration.instance(true).load do
-  def run_remote_rake(rake_cmd)
-    rake_args = ENV['RAKE_ARGS'].to_s.split(',')
-
-    cmd = "cd #{fetch(:latest_release)} && bundle exec #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
-    cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
-    run cmd
-    set :rakefile, nil if exists?(:rakefile)
-  end
-
   namespace :deploy do
     desc "Restart Resque Workers"
-    task :restart_workers, :roles => :worker do
-      run_remote_rake "resque:restart_workers"
+    task :restart_workers, :roles => :daemon do
+       run "#{source_rvmrc} && RAILS_ENV=#{rails_env} bundle exec resque:restart_workers" if use_resque
     end
   end
   
