b915319b06ae110f8888b675ac74ca52a294d207
Bob Breznak <bob.breznak@gmail.com>
2012-08-01 09:47:42 -0400

    Correcting resque task

--- lib/swallow/resque.rb
+++ lib/swallow/resque.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(true).load do
   namespace :deploy do
     desc "Restart Resque Workers"
     task :restart_workers, :roles => :daemon do
-       run "RAILS_ENV=#{rails_env} bundle exec rake resque:restart_workers" if use_resque
+       run "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec rake resque:restart_workers" if use_resque
     end
   end
 
