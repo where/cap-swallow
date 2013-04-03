673d3be58cbb45b2b868addbc18c62f3f77d210a
Jack Thorne <jthorne@where.com>
2013-04-03 16:02:47 -0400

    fix sed script

--- lib/swallow/service.rb
+++ lib/swallow/service.rb
@@ -5,7 +5,7 @@ Capistrano::Configuration.instance(true).load do
     task :setup, :roles => :app do
       puts "  * Installing service to init.d"
       run 'cp #{latest_release}/lib/campaign_manager #{shared_path}/system/campaign_manager'
-      run "sed -i '' 's/__ENV__/#{rails_env}/g' #{shared_path}/system/campaign_manager"
+      run "sed -i 's/__ENV__/#{rails_env}/g' #{shared_path}/system/campaign_manager"
       run 'ln -s #{latest_release}/campaign_manager /etc/init.d/campaign_manager'
     end
 
