5444e3afdd3ae9c56c6696abc23cadc7ab79027d
Jack Thorne <jthorne@where.com>
2013-04-04 18:02:15 -0400

    fix symlink

--- lib/swallow/service.rb
+++ lib/swallow/service.rb
@@ -7,7 +7,7 @@ Capistrano::Configuration.instance(true).load do
         puts "  * Installing service to init.d"
         service_file = ERB.new(File.new(Rails.root.join( 'lib', "#{latest_release}/lib/#{application}.erb")).read, nil, "%").result()
         File.new("#{shared_path}/system/#{application}", "w") { |f| f.write(service_file) }
-        File.symlink("#{shared_path}/system/#{application}", "/etc/init.d/")
+        File.symlink("#{shared_path}/system/#{application}", "/etc/init.d/#{application}")
       end
     end
 
