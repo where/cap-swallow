a7cc348d5fb0df1e73de14e57fa19309d194ff49
Jack Thorne <jthorne@where.com>
2013-04-04 17:25:18 -0400

    change file stuff to ruby and add .erb

--- lib/swallow/service.rb
+++ lib/swallow/service.rb
@@ -3,11 +3,11 @@ Capistrano::Configuration.instance(true).load do
 
     desc "links services to the init.d directory"
     task :setup, :roles => :app do
-      if File.exists? "#{latest_release}/lib/#{application}"
+      if File.exists? "#{latest_release}/lib/#{application}.erb"
         puts "  * Installing service to init.d"
-        run 'cp #{latest_release}/lib/#{application} #{shared_path}/system/#{application}'
-        run "sed -i 's/__ENV__/#{rails_env}/g' #{shared_path}/system/#{application}"
-        run 'ln -s #{latest_release}/#{application} /etc/init.d/#{application}'
+        service_file = ERB.new(File.new(Rails.root.join( 'lib', "#{latest_release}/lib/#{application}.erb")).read, nil, "%").result()
+        File.new("#{shared_path}/system/#{application}", "w") { |f| f.write(service_file) }
+        File.symlink("#{shared_path}/system/#{application}", "/etc/init.d/")
       end
     end
 
