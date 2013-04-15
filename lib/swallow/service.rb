c2a45662414dcdc1d491532769022bedd93478c5
Jack Thorne <jthorne@where.com>
2013-04-15 16:30:14 -0400

    fix serverside placment of files

--- lib/swallow/service.rb
+++ lib/swallow/service.rb
@@ -3,14 +3,13 @@ Capistrano::Configuration.instance(true).load do
 
     desc "links services to the init.d directory"
     task :setup, :roles => :app do
-      if File.exists? "#{latest_release}/lib/#{application}.erb"
-        puts "  * Installing service to init.d"
-        service_file = ERB.new(File.new(Rails.root.join( 'lib', "#{latest_release}/lib/#{application}.erb")).read, nil, "%").result()
-        File.new("#{shared_path}/system/#{application}", "w") { |f| f.write(service_file) }
-        File.symlink("#{shared_path}/system/#{application}", "/etc/init.d/#{application}")
-      end
+      puts "  * Installing service to init.d"
+      # generate service file
+      run "cd #{latest_release} && bundle exec rake service:setup /etc/init.d/ #{shared_path} #{current_path} RAILS_ENV=#{rails_env}"
+      run "cp #{latest_release}/tmp/system/#{application} #{shared_path}/system/#{application}"
     end
 
     after "deploy", "service:setup"
   end
 end
+
