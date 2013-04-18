c6fdbdad3ec07a819a02411d5ce8960eb21d7f21
Andy Chambers <anchambers@paypal.com>
2013-04-18 17:45:37 +0200

    Run restart_workers on the correct host

--- lib/swallow/resque.rb
+++ lib/swallow/resque.rb
@@ -4,7 +4,8 @@ Capistrano::Configuration.instance(true).load do
     task :restart_workers, :roles => :resque do
       find_servers_for_task(current_task).each do |current_server|
         puts "starting workers on #{current_server.host}"
-        run "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec rake resque:restart_workers[#{current_server.host}]" if use_resque
+        cmd = "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec rake resque:restart_workers[#{current_server.host}]"
+        run(cmd, :hosts => current_server.host) if use_resque
       end
     end
   end
